45c6ff6c10c7db7f33aacffed4a7fde508d47505
MohammedSaadQ <malqarni@sbm.net.sa>
2024-09-08 18:14:55 +0300

    Update appv6.py

--- appv6.py
+++ appv6.py
@@ -1,4 +1,3 @@
-
 from flask import Flask, request, jsonify
 import requests
 import logging
@@ -81,10 +80,13 @@ def get_ibm_token():
 @app.route('/recommendation', methods=['POST'])
 def recommendation():
     try:
+        params = request.json
+        customer_id = params.get("id")
+        Location = params.get("location")
         # Get the IBM Cloud OAuth token
         mltoken = get_ibm_token()
         # Construct the question
-        question = "recommend 3 dishes for customer with ID = 1 that share the same location as the customer (Riyadh) and do not recommend dishes that share ingredients with the customer's dislikes and allergies. recommend dishes that share ingredients with the customer's preferences. recommend dishes whose calories do not exceed the customer's calorie limit. recommend dishes whose price is less than or equal to the customer's budget. only respond with the dish id and name and restaurant name and calories of dish and price of dish"
+        question = "recommend 3 dishes for customer with ID = {customer_id} that share the same location as the customer {Location} and do not recommend dishes that share ingredients with the customer's dislikes and allergies. recommend dishes that share ingredients with the customer's preferences. recommend dishes whose calories do not exceed the customer's calorie limit. recommend dishes whose price is less than or equal to the customer's budget. only respond with the dish id and name and restaurant name and calories of dish and price of dish"
         # Prepare the messages payload
         messages = [{"role": "user", "content": question}]
         # Prepare the header
